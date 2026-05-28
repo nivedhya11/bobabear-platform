f048e098cd599b6dfc6d1cfa2d0e828eb7e79b0b
Ashutosh Joshi <ajoshi@redcloudcomputing.com>
2026-05-28 10:54:54 +0000

    Minor fixes

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -14,22 +14,23 @@
 
 import Script from "next/script";
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export function Analytics() {
-  if (!GA_ID) return null;
+  if (!GA_MEASUREMENT_ID) return null;
 
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
-      <Script id="ga-init" strategy="afterInteractive">{`
+      <Script id="google-analytics" strategy="afterInteractive">{`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
+        window.gtag = window.gtag || gtag;
         gtag('js', new Date());
-        gtag('config', '${GA_ID}', { page_path: window.location.pathname });
+        gtag('config', '${GA_MEASUREMENT_ID}');
       `}</Script>
     </>
   );
