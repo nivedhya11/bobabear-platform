39679d0e4f0028b98207dc15a4d776d44e0d942a
nivedhya11 <nivedhya11@users.noreply.github.com>
2026-09-06 04:50:22 +0000

    deploy: b615fb20e034d71f49a1fa588f976d0592a46098

---
+++ _next/static/chunks/app/(customer)/privacy/page-e0af81f0dda9ac63.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[795],{32553:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,98500,23))}},e=>{e.O(0,[8500,8441,3794,7358],()=>e(e.s=32553)),_N_E=e.O()}]);
